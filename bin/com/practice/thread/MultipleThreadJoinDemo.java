f3cfa23524c34bab7117d6f5669ff7ed1e77d276
Diganta Banerjee <diganta4@gmail.com>
2014-07-04 10:08:04 +0530

    Add Thread Class

---
+++ bin/com/practice/thread/MultipleThreadJoinDemo.java
@@ -0,0 +1,20 @@
+package com.practise.thread;
+
+public class MultipleThreadJoinDemo {
+
+	public static void main(String[] args) {
+		//Creating multiple threads
+		MultipleThread mt1 = new MultipleThread("One");
+		MultipleThread mt2 = new MultipleThread("Two");
+		MultipleThread mt3 = new MultipleThread("Three");
+		
+		//wait for threads to finish
+		try {
+			mt1.t.join();
+			mt2.t.join();
+			mt3.t.join();
+		} catch (InterruptedException ie) {
+			System.out.println("Main thread interrupted");
+		}
+	}
+}
